fdcc1b0a88f975ee118e970a040993f87bd668bc
Oliwia Jędrzejec <oliwiajedrzejec@gmail.com>
2024-10-06 01:41:27 +0200

    Solve 413 error: request entity too large => Increase the size limit for the JSON payload

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,15 @@ const orchestraMemberRoutes = require('./src/orchestra_member/routes');
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+// Increase the size limit for the JSON payload
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));  // Increase the limit for URL-encoded payloads
 
 app.get('/', (req, res) => {
-  res.send("Hello World")
+  res.send("Hello World");
 });
 
 app.use('/api/orchestra', orchestraRoutes);
 app.use('/api/orchestraMember', orchestraMemberRoutes);
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
-
