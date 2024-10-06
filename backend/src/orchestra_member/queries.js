cdc248c27239112fa452925d9012257e1c0648f9
Oliwia Jędrzejec <oliwiajedrzejec@gmail.com>
2024-10-06 23:49:19 +0200

    Cleanup

--- backend/src/orchestra_member/queries.js
+++ backend/src/orchestra_member/queries.js
@@ -24,26 +24,10 @@ const createOrchestraMember = `
 
 const deleteOrchestraMemberById = 'DELETE FROM orchestra_member WHERE id = $1'
 
-// const updateOrchestraMemberById = `
-//     UPDATE orchestra_member
-//     SET
-//         first_name = $2,
-//         last_name = $3,
-//         phone = $4,
-//         birth_date = $5,
-//         are_you_student = $6,
-//         university = $7,
-//         profile_picture = $8,
-//         description = $9
-//     WHERE id = $1
-//     RETURNING *;
-// `
-
 module.exports = {
     getOrchestraMembers,
     getOrchestraMemberById,
     checkEmailExists,
     createOrchestraMember,
     deleteOrchestraMemberById,
-    // updateOrchestraMemberById,
 }
