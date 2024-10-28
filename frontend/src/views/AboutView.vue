2e4e32c070fdb61f349cdb65b97e6601ec1c190b
Oliwia Jędrzejec <oliwiajedrzejec@gmail.com>
2024-10-28 14:20:03 +0100

    Add an empty frontend design

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about-view">
+    <h1>This is an about view</h1>
   </div>
 </template>
 
+<script setup lang="ts">
+</script>
+
 <style>
-@media (min-width: 1024px) {
-  .about {
+/* @media (min-width: 1024px) {
+  .about-view {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
-}
+} */
 </style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,18 @@
+<template>
+  <div class="home-view">
+    <h1>This is a home view</h1>
+  </div>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+/* @media (min-width: 1024px) {
+  .home-view {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+} */
+</style>
