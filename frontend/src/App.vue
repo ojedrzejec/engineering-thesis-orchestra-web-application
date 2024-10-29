2b29ba7bbce1266e00eb9de8f0b7a3d3375d2370
Oliwia Jędrzejec <oliwiajedrzejec@gmail.com>
2024-10-29 16:33:10 +0100

    Add login form layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,7 +10,6 @@
         <br />
         <RouterLink to="/registration">Registration</RouterLink>
       </nav>
-      <Button label="Test Button"></Button>
     </div>
   </header>
   <RouterView />
@@ -18,7 +17,6 @@
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import Button from 'primevue/button';
 </script>
 
 <style>
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,10 +1,33 @@
 <template>
   <div class="login">
-    <h1>This is an login page</h1>
+    <h1>Login to your account</h1>
+  </div>
+  <div>
+    <FloatLabel variant="in">
+      <InputText id="username" v-model="username" :invalid="!validationUsername" ></InputText>
+      <label for="username">Username</label>
+    </FloatLabel>
+    <FloatLabel variant="in">
+      <InputText id="password" v-model="password" :invalid="!validationPassword" ></InputText>
+      <label for="password">Password</label>
+    </FloatLabel>
+    <Button label="Login"></Button>
+  </div>
+  <div>
+    Don't have an account? <RouterLink to="/registration">Register</RouterLink>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import FloatLabel from 'primevue/floatlabel';
+
+const username = ref('');
+const validationUsername = ref('');
+const password = ref('');
+const validationPassword = ref('');
 </script>
 
 <style>
