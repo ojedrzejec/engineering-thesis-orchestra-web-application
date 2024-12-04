c612e25eb971fb9460b5700a3d783f303bf62f79
Oliwia Jędrzejec <oliwiajedrzejec@gmail.com>
2024-12-04 18:26:21 +0100

    Display orchestra name and id in AvailabilityView using available orchestras store

--- frontend/src/views/logged-in-user/orchestra-member/AvailabilityView.vue
+++ frontend/src/views/logged-in-user/orchestra-member/AvailabilityView.vue
@@ -1,28 +1,23 @@
 <template>
   <div class="availability-view">
     <div class="availability-view__title">
-      <h1>My Availability in {{ orchestraInformation?.name }}</h1>
+      <h1>My Availability in {{ selectedOrchestraDetails?.name }}</h1>
     </div>
+    <h3>My Availability in {{ selectedOrchestraId }}</h3>
     {{ route.params }}
   </div>
 </template>
 
 <script setup lang="ts">
-import { watch } from 'vue'
 import { useRoute } from 'vue-router'
-import { useOrchestraInformation } from '@/composables/useOrchestraInformation'
+import { storeToRefs } from 'pinia'
+import { useAvailableOrchestrasStore } from '@/stores/useAvailableOrchestras'
 
 const route = useRoute()
 
-const { fetchOrchestraInformation, orchestraInformation } =
-  useOrchestraInformation()
-
-watch(
-  () => route.params.orchestraId,
-  async orchestraId => {
-    await fetchOrchestraInformation(orchestraId.toString())
-  },
-  { immediate: true },
+const availableOrchestrasStore = useAvailableOrchestrasStore()
+const { selectedOrchestraId, selectedOrchestraDetails } = storeToRefs(
+  availableOrchestrasStore,
 )
 </script>
 
