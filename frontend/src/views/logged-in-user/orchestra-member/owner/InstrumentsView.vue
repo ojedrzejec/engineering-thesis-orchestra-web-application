1e26224cbd9777ff6b4619517bd8e9bc2b744896
Oliwia Jędrzejec <oliwiajedrzejec@gmail.com>
2024-12-06 22:04:28 +0100

    Add DataTable component with search window to display instruments in an appealing way for user

--- frontend/src/views/logged-in-user/orchestra-member/owner/InstrumentsView.vue
+++ frontend/src/views/logged-in-user/orchestra-member/owner/InstrumentsView.vue
@@ -4,21 +4,89 @@
       <h1>Instruments</h1>
     </div>
 
-    <pre>
-      {{ route.params }}
-    </pre>
-    <pre>
-      {{ { instrumentsInOrchestra } }}
-    </pre>
     <ProgressSpinner v-if="loadingInstrumentsInOrchestra" />
+
+    <div class="instruments-view__datatable">
+      <DataTable
+        v-model:filters="filters"
+        filterDisplay="row"
+        :globalFilterFields="[
+          'instrumentName',
+          'orchestraMemberFirstName',
+          'orchestraMemberLastName',
+          'orchestraMemberEmail',
+        ]"
+        :value="instrumentsInOrchestra"
+        :loading="loadingInstrumentsInOrchestra"
+        removableSort
+        tableStyle="min-width: 50rem"
+      >
+        <template #header>
+          <div class="flex justify-end">
+            <IconField>
+              <InputIcon>
+                <i class="pi pi-search"></i>
+              </InputIcon>
+              <InputText
+                v-model="filters['global'].value"
+                placeholder="Keyword Search"
+              />
+            </IconField>
+          </div>
+        </template>
+        <template #empty> No customers found. </template>
+        <template #loading> Loading customers data. Please wait. </template>
+        <Column
+          field="instrumentName"
+          header="Instrument"
+          sortable
+          style="width: 25%; font-weight: bold"
+        >
+          <template #body="{ data }">
+            {{ data.instrumentName }}
+          </template>
+          <template #filter="{ filterModel, filterCallback }">
+            <InputText
+              v-model="filterModel.value"
+              type="text"
+              @input="filterCallback()"
+              placeholder="Search by instrument name"
+            />
+          </template>
+        </Column>
+        <Column
+          field="orchestraMemberFirstName"
+          header="First Name"
+          sortable
+          style="width: 25%"
+        ></Column>
+        <Column
+          field="orchestraMemberLastName"
+          header="Last Name"
+          sortable
+          style="width: 25%"
+        ></Column>
+        <Column
+          field="orchestraMemberEmail"
+          header="Email"
+          style="width: 25%"
+        ></Column>
+      </DataTable>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useInstruments } from '@/composables/useInstruments'
-import { watch } from 'vue'
+import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import ProgressSpinner from 'primevue/progressspinner'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import { FilterMatchMode } from '@primevue/core/api'
+import IconField from 'primevue/iconfield'
+import InputIcon from 'primevue/inputicon'
+import InputText from 'primevue/inputtext'
 
 const route = useRoute()
 
@@ -27,6 +95,11 @@ const {
   loadingInstrumentsInOrchestra,
   fetchInstrumentsInOrchestra,
 } = useInstruments()
+
+const filters = ref({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  instrumentName: { value: null, matchMode: FilterMatchMode.CONTAINS },
+})
 
 watch(
   () => route.params.orchestraId,
