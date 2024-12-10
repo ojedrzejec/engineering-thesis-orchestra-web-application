ace151993c2c735b24f932066e0526235458c874
Oliwia Jędrzejec <oliwiajedrzejec@gmail.com>
2024-12-10 14:32:00 +0100

    Display all orchestra concerts in separate card; add concert details in right positioned drawer

---
+++ frontend/src/components/ConcertDetails.vue
@@ -0,0 +1,92 @@
+<template>
+  <div v-if="concertDetails" class="concert-details-view__drawer-content">
+    <div
+      class="concert-details-view__drawer-single-info concert-details-view__drawer-graphic"
+    >
+      <div v-if="concertDetails.graphic">
+        <img
+          :src="concertDetails.graphic"
+          alt="Profile Picture"
+          style="
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            border-radius: 0.75rem;
+            width: 100%;
+            max-width: 10rem;
+          "
+        />
+      </div>
+    </div>
+    <div class="concert-details-view__drawer-single-info">
+      <div class="concert-details-view__drawer-single-info">
+        <h4>Concert Name:</h4>
+        <p v-if="concertDetails.name">
+          {{ concertDetails.name }}
+        </p>
+        <div
+          class="concert-details-view__drawer-single-info-not-provided"
+          v-if="!concertDetails.name"
+        >
+          Not provided
+        </div>
+      </div>
+
+      <div class="concert-details-view__drawer-single-info">
+        <h4>Description:</h4>
+        <p v-if="concertDetails.description">
+          {{ concertDetails.description }}
+        </p>
+        <div
+          class="concert-details-view__drawer-single-info-not-provided"
+          v-if="!concertDetails.description"
+        >
+          Not provided
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { TConcert } from '@/types/TConcert'
+
+const props = defineProps<{
+  concertDetails: TConcert | null
+}>()
+</script>
+
+<style setup lang="scss">
+.concert-details-view {
+  //   &__drawer-content {
+  //     display: flex;
+  //     flex-direction: column;
+  //     // gap: 10px;
+  //     // width: 30rem;
+  //     // height: 100%;
+  //   }
+
+  //   &__drawer-single-info {
+  //     // display: flex;
+  //     // flex-direction: row;
+  //     // align-items: center;
+  //     // gap: 20px;
+
+  //     display: grid;
+  //     grid-template-columns: 1fr 1fr;
+  //     gap: 20px;
+  //     align-items: center;
+  //   }
+
+  //   &__drawer-graphic {
+  //     font-weight: bold;
+  //     display: flex;
+  //     align-items: center;
+  //     justify-content: center;
+  //     margin: 20px auto;
+  //     // border: 1px solid #e0e0e0;
+  //   }
+
+  //   &__drawer-single-info-not-provided {
+  //     color: #b8b8b8;
+  //   }
+}
+</style>
--- frontend/src/views/logged-in-user/orchestra-member/owner-and-manager/ConcertsView.vue
+++ frontend/src/views/logged-in-user/orchestra-member/owner-and-manager/ConcertsView.vue
@@ -1,35 +1,53 @@
 <template>
   <div class="concerts-view">
+    <Toast />
     <div class="concerts-view__title">
       <h1>Concerts</h1>
     </div>
 
-    <div class="concerts-view__content">
-      <div class="concerts-view__component-create card flex justify-center">
-        <Drawer
-          v-model:visible="visible"
-          header="Create new concert"
-          position="full"
-        >
-          <ConcertCreateForm />
-        </Drawer>
-        <div>
-          <h3>Create new concert</h3>
-          <Button icon="pi pi-plus" @click="visible = true"></Button>
+    <div class="concerts-view__component-create card flex justify-center">
+      <Drawer
+        v-model:visible="visibleDrawerConcertCreateForm"
+        header="Create new concert"
+        position="full"
+      >
+        <ConcertCreateForm />
+      </Drawer>
+      <div>
+        <h3>Create new concert</h3>
+        <div class="concerts-view__component-create-button">
+          <Button
+            size="large"
+            icon="pi pi-plus"
+            @click="visibleDrawerConcertCreateForm = true"
+          ></Button>
         </div>
       </div>
+    </div>
+    <div class="concerts-view__content">
+      <div v-if="loadingConcerts">
+        <ProgressSpinner />
+      </div>
+      <div v-else-if="!concerts">
+        <Message severity="error">Failed to load concerts.</Message>
+      </div>
 
-      <div class="concerts-view__concert-list">
+      <div v-else class="concerts-view__concert-list">
         <h3>Concerts</h3>
-        <div class="concerts-view__concert-details">
-          <Card style="width: 33rem; overflow: hidden">
+        <div
+          v-for="(concert, id) in concerts"
+          :key="id"
+          class="concerts-view__concert-details"
+        >
+          <Card style="max-width: 33rem; overflow: hidden">
             <template #header>
-              <!-- <img
-              alt="user header"
-              src="https://primefaces.org/cdn/primevue/images/usercard.png"
-            /> -->
+              <img
+                alt="concert graphic"
+                :src="concert.graphic ?? 'https://placehold.co/'"
+                class="concerts-view__concert-image"
+              />
             </template>
-            <template #title>Concert Title</template>
+            <template #title>{{ concert.name }}</template>
             <template #subtitle>
               <!-- Card subtitle -->
               <div>
@@ -38,70 +56,111 @@
                     <i class="pi pi-calendar" style="color: #708090"></i>
                     DATE:
                   </div>
-                  <div><strong>01.03.2025</strong></div>
+                  <div>
+                    <strong>{{ concert.date_and_time }}</strong>
+                  </div>
                 </div>
                 <div class="concerts-view__concert-details-subtitle">
                   <div>
                     <i class="pi pi-clock" style="color: #708090"></i>
                     HOUR:
                   </div>
-                  <div><strong>19:00</strong></div>
+                  <div>
+                    <strong>{{ concert.date_and_time }}</strong>
+                  </div>
                 </div>
                 <div class="concerts-view__concert-details-subtitle">
                   <div>
                     <i class="pi pi-map-marker" style="color: #708090"></i>
                     LOCATION:
                   </div>
-                  <div><strong>Filharmonia Łódzka</strong></div>
+                  <div>
+                    <strong>{{ concert.place }}</strong>
+                  </div>
                 </div>
               </div>
             </template>
             <template #content>
-              <p class="m-0">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Inventore sed consequuntur error repudiandae numquam deserunt
-                quisquam repellat libero asperiores earum nam nobis, culpa
-                ratione quam perferendis esse, cupiditate neque quas!
-              </p>
+              {{ concert.description }}
             </template>
             <template #footer>
               <div class="concerts-view__single-button flex gap-4 mt-1">
-                <Button label="See details" class="w-full"></Button>
+                <div class="card flex justify-center">
+                  <Drawer
+                    v-model:visible="visibleDrawerConcertDetails"
+                    position="right"
+                    header="Concert Details"
+                    class="concerts-view__drawer !w-full md:!w-80 lg:!w-[30rem]"
+                  >
+                    <ConcertDetails :concertDetails="selectedConcert" />
+                  </Drawer>
+                  <Button
+                    label="See details"
+                    @click="openConcertDetails(concert)"
+                  ></Button>
+                </div>
               </div>
             </template>
           </Card>
         </div>
+
+        <pre>
+          <div>Create a concerts (seperate component)</div>
+          
+          <div>Display all concerts -> go to details (seperate component)</div>
+          
+          <div>concert details component:
+            - edit button on the top right -> go to edit concert (seperate component)
+            - delete button on the top right
+            - concert details
+            - list of all members with their availability (seperate component)
+          </div>
+  
+          <div>edit concert component:
+            - form
+            - save button
+          </div>
+        </pre>
       </div>
-
-      <pre>
-        <div>Create a concerts (seperate component)</div>
-        
-        <div>Display all concerts -> go to details (seperate component)</div>
-        
-        <div>concert details component:
-          - edit button on the top right -> go to edit concert (seperate component)
-          - delete button on the top right
-          - concert details
-          - list of all members with their availability (seperate component)
-        </div>
-
-        <div>edit concert component:
-          - form
-          - save button
-        </div>
-      </pre>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
 import Drawer from 'primevue/drawer'
 import Button from 'primevue/button'
 import Card from 'primevue/card'
+import Toast from 'primevue/toast'
+import ProgressSpinner from 'primevue/progressspinner'
 import ConcertCreateForm from '@/components/ConcertCreateForm.vue'
+import ConcertDetails from '@/components/ConcertDetails.vue'
+import { useConcerts } from '@/composables/useConcerts'
+import type { TConcert } from '@/types/TConcert'
 
-const visible = ref(false)
+const route = useRoute()
+
+const visibleDrawerConcertCreateForm = ref(false)
+const visibleDrawerConcertDetails = ref(false)
+const selectedConcert = ref<TConcert | null>(null)
+
+const { concerts, loadingConcerts, fetchConcerts } = useConcerts()
+
+watch(
+  () => route.params.orchestraId,
+  async orchestraId => {
+    await fetchConcerts(orchestraId.toString())
+  },
+  { immediate: true },
+)
+
+const openConcertDetails = (concert: TConcert) => {
+  selectedConcert.value = concert
+  if (selectedConcert.value !== null) {
+    visibleDrawerConcertDetails.value = true
+  }
+}
 </script>
 
 <style setup lang="scss">
@@ -118,6 +177,10 @@ const visible = ref(false)
     width: 100%;
   }
 
+  &__component-create-button {
+    padding-left: 4rem;
+  }
+
   &__concert-details-subtitle {
     display: flex;
     flex-direction: row;
@@ -128,6 +191,17 @@ const visible = ref(false)
     display: flex;
     flex-direction: row;
     justify-content: end;
+  }
+
+  &__concert-image {
+    width: 100%;
+    max-height: 300px;
+    object-fit: cover;
+  }
+
+  &__drawer {
+    width: 100% !important;
+    max-width: 30rem;
   }
 }
 </style>
