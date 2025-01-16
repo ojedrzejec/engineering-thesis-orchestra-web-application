b6fae262f94bec84e8566158a98ef846d0c278c1
Oliwia Jędrzejec <oliwiajedrzejec@gmail.com>
2025-01-16 19:27:37 +0100

    Add useWidthStore store

---
+++ frontend/src/stores/useWidthStore.ts
@@ -0,0 +1,21 @@
+import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
+
+export const useWidthStore = defineStore('width', () => {
+  const width = ref<number>(window.innerWidth)
+
+  window.addEventListener('resize', () => {
+    width.value = window.innerWidth
+  })
+
+  // () => width.value < 768
+  const isMobile = computed(() => width.value < 768)
+
+  const isDesktop = computed(() => width.value >= 768)
+
+  return {
+    width,
+    isMobile,
+    isDesktop,
+  }
+})
